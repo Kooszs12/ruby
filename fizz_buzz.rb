d93a3e37a91fba9151d9bbb32f3c164592b52cec
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-06-05 10:43:39 +0000

    [Finish] 6/5 2-8

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,5 +1,13 @@
 def fizz_buzz(number)
-  
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif  number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 ==0
+    puts "Buzz"
+  else
+    puts number
+  end
 end
 
 puts "数字を入力してください。"
