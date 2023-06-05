d75590277d337d22142ca94c5dee5c1aef57b56f
EC2 Default User <ec2-user@ip-172-31-9-210.ap-northeast-1.compute.internal>
2023-06-05 11:51:19 +0000

    [Fix] 6/5 2-8

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,12 +1,12 @@
 def fizz_buzz(number)
   if number % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif  number % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif number % 5 ==0
-    puts "Buzz"
+    "Buzz"
   else
-    puts number
+    puts number.to_s
   end
 end
 
